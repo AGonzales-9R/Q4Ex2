37c0894472a20e2116c58c43656b1767f2705a9c
AGonzales-9R <gonzales.am@obmontessori.edu.ph>
2025-04-11 08:52:22 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function verifyAge(){
+	var age = document.getElementById('inputAge').value;
+
+	if (age<18) {
+
+		window.alert("You are not eligible to vote.");
+		
+	} else {
+
+		window.alert("Welcome! You are eligible to vote!");
+
+	}
+	
+	}
+
+window.alert("LOADING")
